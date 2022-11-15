8d1bfca6cb197b170d7403fb4550a22ae7b3de50
Jason Harley <jason@honeycomb.io>
2022-11-15 11:14:57 -0500

    Initial commit of Kinesis Firehose template (#1)
    Intended to be used as a nested stack for CloudWatch Logs and CloudWatch metrics templates.

---
+++ scripts/publish_aws.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ships templates to S3 in ~all regions in the "honeycomb-integrations-$REGION" bucket
+
+set -e
+
+REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1"
+
+# if DRYRUN is set to anything, turn it into the awscli switch
+[[ -n "${DRYRUN}" ]] && DRYRUN="--dryrun"
+
+echo "+++ Uploading templates"
+for REGION in ${REGIONS}; do
+	DEPLOY_ROOT=s3://honeycomb-integrations-${REGION}/cloudformation-templates
+	aws s3 sync ${DRYRUN} templates/ "${DEPLOY_ROOT}"
+done
